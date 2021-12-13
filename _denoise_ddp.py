04b3d96ade652496a313c7f77abd1dae5192f0f0
KHOA TRAN <48320778+khoatranrb@users.noreply.github.com>
2021-12-13 11:37:08 +0700

    Update _denoise_ddp.py

--- _denoise_ddp.py
+++ _denoise_ddp.py
@@ -4,120 +4,19 @@ from torch.utils.data import DataLoader, Dataset
 import matplotlib.pyplot as plt
 import cv2
 import os
-
-class Data(Dataset):
-    def __init__(self, path, train=True):
-        self.train = train
-        self.path = os.path.join(path, 'val')
-        if train:
-            self.path = os.path.join(path, 'train')
-
-    def __len__(self):
-        return 80
-        if self.train:
-            return len(os.listdir(self.path))
-        return len(os.listdir(os.path.join(self.path, 'inp')))
-    
-    def __getitem__(self, idx):
-        idx -= 1
-        if self.train:
-            path = os.path.join(self.path, '%s.jpg'%(idx))
-            out = cv2.imread(path)
-            out = cv2.resize(out, (128,128))
-            noise = np.random.normal(0,40,out.shape)
-            inp = out.astype(float) + noise
-            inp = np.clip(inp,0,255)
-        else:
-            inppath = os.path.join(self.path, 'inp', '%s.jpg'%(idx))
-            outpath = os.path.join(self.path, 'out', '%s.jpg'%(idx))
-            out = cv2.imread(outpath)
-            inp = cv2.imread(inppath)
-            out = cv2.resize(out, (128,128))
-            inp = cv2.resize(inp, (128,128))
-            
-
-        inp = inp.astype(np.uint8).transpose((2,0,1))
-        out = out.transpose((2,0,1))
-
-        inp = inp/127.5-1
-        out = out/127.5-1
-
-        return inp.astype(np.float32), out.astype(np.float32)
+from dataloaders import Denoise_Data
+import torch.nn as nn
+from models import DPED_DDP
 
 bs = 8
 
-trainset = Data('data', train= True)
+trainset = Denoise_Data('data', train= True)
 trainloader = DataLoader(trainset, shuffle=True, batch_size=bs)
 
-valset = Data('data', train=not True)
+valset = Denoise_Data('data', train=not True)
 valloader = DataLoader(valset, batch_size=bs)
 
-import torch.nn as nn
-from ddp import Step
-
-class DPED(nn.Module):
-    def __init__(self, out_channels=64):
-        super(DPED, self).__init__()
-        self.conv1 = Step(nn.Conv2d(3, out_channels, 9, padding=4))
-        
-        self.block1 = ConvBlock(64, 64, 3)
-        self.block2 = ConvBlock(64, 64, 3)
-        self.block3 = ConvBlock(64, 64, 3)
-        self.block4 = ConvBlock(64, 64, 3)
-        
-        self.conv2 = Step(nn.Conv2d(64, 64, 3, padding=1))
-        self.conv3 = Step(nn.Conv2d(64, 64, 3, padding=1))
-        self.conv4 = Step(nn.Conv2d(64, 3, 9, padding=4))
-        # self.conv2 = nn.Conv2d(64, 64, 3, padding=1)
-        # self.conv3 = nn.Conv2d(64, 64, 3, padding=1)
-        # self.conv4 = nn.Conv2d(64, 3, 9, padding=4)
-        self.activation = nn.Tanh()
-        
-        self.relu1 = nn.ReLU()
-        self.relu2 = nn.ReLU()
-        self.relu3 = nn.ReLU()
-        
-    def forward(self, x=None, update=False, opt=None):
-        out = self.conv1(x, update, opt)
-        out = self.relu1(out)
-        
-        out = self.block1(out, update, opt)
-        out = self.block2(out, update, opt)
-        out = self.block3(out, update, opt)
-        out = self.block4(out, update, opt)
-        
-        out = self.conv2(out, update, opt)
-        out = self.relu2(out)
-        
-        out = self.conv3(out, update, opt)
-        out = self.relu3(out)
-
-        out = self.conv4(out, update, opt)
-        out = self.activation(out)
-        
-        return out
-
-class ConvBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, conv_size):
-        super(ConvBlock, self).__init__()
-        self.conv_size = conv_size
-        
-        self.conv1 = Step(nn.Conv2d(in_channels, out_channels, conv_size, 1, padding=1))
-        self.conv2 = Step(nn.Conv2d(in_channels, out_channels, conv_size, 1, padding=1))
-        
-        self.relu = nn.ReLU()
-        
-    def forward(self, x=None, update=False, opt=None):
-        out = self.conv1(x, update, opt)
-        out = self.relu(out)
-        
-        out = self.conv2(out, update, opt)
-        out = self.relu(out)
-        
-        out = out + x
-        return out
-
-model = DPED().cuda()
+model = DPED_DDP().cuda()
 
 from ddp import DDPNOPT
 
